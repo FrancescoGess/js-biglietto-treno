cd7d7eaca29379757b607991de90ac044543ac79
FrancescoGess <148055980+FrancescoGess@users.noreply.github.com>
2023-12-27 15:34:29 +0100

    complete

--- main.js
+++ main.js
@@ -7,7 +7,29 @@
 //per indicare centesimi sul prezzo). Questo richiederà un minimo di ricerca.**
 
 function myFunction() {
-    let km = prompt("Quanti km le servono per arrivare a destinazione", "50");
-    let age = prompt("Quanti anni hai", "18")
+    let km = (prompt)("Quanti km le servono per arrivare a destinazione", "50");
+    let age = (prompt)("Quanti anni hai", "18")
+    let ticketPrice;
+    const kmCost = 0.21;
   }
 
+  document.writeln(
+    `
+    Vuoi percorrere: ${km}
+    L'età inserita è: ${age}
+    `
+);
+
+
+if ( age < 18 ) {
+    ticketPrice = ((km * kmCost) - ((km * kmCost) * 20) / 100);
+    document.writeln(`Il costo del biglietto è: € ${ticketPrice.toFixed(2)}`);
+} else if (age >= 65) {
+    ticketPrice = ((km * kmCost) - ((km * kmCost) * 40) / 100);
+    document.writeln(`Il costo del biglietto è: € ${ticketPrice.toFixed(2)}`);
+} else {
+    ticketPrice = (km * kmCost);
+    document.writeln(`Il costo del biglietto è: € ${ticketPrice.toFixed(2)}`);
+}
+
+console.log(`Il costo del biglietto è: ${ticketPrice.toFixed(2)}`);
